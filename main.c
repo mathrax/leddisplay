644194eaa868e9479bd0034db6ab027e9ed35d11
SHOZO KUZE <shozo@kuze.jp>
2016-12-03 19:15:34 +0900

    2016/12/3 ホシノキ、ドリポン

--- main.c
+++ main.c
@@ -1,128 +1,34 @@
 #include <plib.h>
 #include <stdlib.h>
 
-#include "ledmatrix_ws2812.h"
-#include "pattern.h"
-#include "animationA.h"
-#include "animationB.h" //BROKEN
-#include "animationD.h" //NOAMRL BLINK
-#include "animationF.h"
-#include "animationJ.h" //WAVE
-#include "animationL.h" //HANABI
-//#include "suika.h"
-#include "sleep_heart.h" //SLEEP HEART
-//#include "star.h" //STAR
-//#include "garapiko17.h" //KAERU,ONPU,KARASU
-//#include "patolamp_hasami.h" //KAMIFUBUKI,JAJAN
-//#include "cho2.h" //
-//#include "chulip.h" //
-//#include "heart_20160810.h" //
-//#include "pencil.h" //
-//#include "leaf_20160830.h" //
-#include "teapot.h"
+#include "./user.h"
+#include "./ledmatrix_ws2812.h"
 
+#include "./animation_data/pattern.h"
+#include "./animation_data/animationA.h"
+#include "./animation_data/animationB.h" //BROKEN
+#include "./animation_data/animationD.h" //NOAMRL BLINK
+#include "./animation_data/animationF.h"
+#include "./animation_data/animationJ.h" //WAVE
+#include "./animation_data/animationL.h" //HANABI
+//#include "./animation_data/suika.h"
+#include "./animation_data/sleep_heart.h" //SLEEP HEART
+//#include "./animation_data/star.h" //STAR
+//#include "./animation_data/garapiko17.h" //KAERU,ONPU,KARASU
+//#include "./animation_data/patolamp_hasami.h" //KAMIFUBUKI,JAJAN
+//#include "./animation_data/cho2.h" //
+//#include "./animation_data/chulip.h" //
+//#include "./animation_data/heart_20160810.h" //
+//#include "./animation_data/pencil.h" //
+//#include "./animation_data/leaf_20160830.h" //
+#include "./animation_data/doripon.h"
+#include "./animation_data/hoshinoki.h"
 
-
-// SYSCLK = 40 MHz (8MHz Crystal/ FPLLIDIV * FPLLMUL / FPLLODIV)
-// PBCLK  = 40 MHz
-#define SYSCLK  40000000L                      //
-#define FCY          SYSCLK
-#define SYSCLKdiv10MHz    (SYSCLK/10000000)    //
-#define GetSystemClock()       (SYSCLK)
-
-
-// Configuration Bit settings
-// DEVCFG3:
-#pragma config IOL1WAY  = OFF           // Peripheral Pin Select Configuration
-// DEVCFG2:
-#pragma config FPLLODIV = DIV_2         // PLL Output Divider
-#pragma config FPLLMUL  = MUL_20        // PLL Multiplier
-#pragma config FPLLIDIV = DIV_2         // PLL Input Divider
-// DEVCFG1:
-#pragma config FWDTEN   = OFF           // Watchdog Timer
-#pragma config WDTPS    = PS1           // Watchdog Timer Postscale
-#pragma config FCKSM    = CSDCMD        // Clock Switching & Fail Safe Clock Monitor
-#pragma config FPBDIV   = DIV_1         // Peripheral Clock divisor
-#pragma config OSCIOFNC = OFF           // CLKO Enable
-#pragma config POSCMOD  = OFF           // Primary Oscillator
-#pragma config IESO     = OFF           // Internal/External Switch-over
-#pragma config FSOSCEN  = OFF           // Secondary Oscillator Enable (KLO was off)
-#pragma config FNOSC    = FRCPLL        // Oscillator Selection
-// DEVCFG0:
-#pragma config CP       = OFF           // Code Protect
-#pragma config BWP      = ON            // Boot Flash Write Protect
-#pragma config PWP      = OFF           // Program Flash Write Protect
-#pragma config ICESEL   = ICS_PGx1      // ICE/ICD Comm Channel Select
-#pragma config JTAGEN   = OFF           // JTAG Enable
-#pragma config DEBUG    = OFF           // Background Debugger Enable
-
-#define neopixel_pin      LATBbits.LATB9
-
-
-unsigned char red[1024];
-unsigned char blu[1024];
-unsigned char grn[1024];
-
-unsigned char myRed;
-unsigned char myGrn;
-unsigned char myBlu;
-
-//PROTOTYPE
-extern void setPixelColor(unsigned int pixel, unsigned char r, unsigned char g, unsigned char b);
-extern void show();
-extern void resetAnimation();
-extern void InitUart1();
-extern void setPattern(const unsigned char* ptn, unsigned char div);
-extern void deletePattern();
-
-#define TRIS_TX1                TRISBbits.TRISB3
-#define TRIS_RX1                TRISBbits.TRISB2
 
 unsigned char aCnt;
 unsigned char frameCount;
 unsigned char firstReset;
 
-void InitUart1() {
-
-    TRIS_TX1 = 0;
-    TRIS_RX1 = 1;
-
-    // Create a UART TX/RX Pin
-    SYSKEY = 0xAA996655; // Write Key1 to SYSKEY
-    SYSKEY = 0x556699AA; // Write Key2 to SYSKEY
-
-    U1RXRbits.U1RXR = 0b0100; //U1RX -- RB2
-    RPB3Rbits.RPB3R = 0b0001; // U1TX -- RB3
-
-    SYSKEY = 0; // Locks the pin Configurations
-
-    U1MODE = 0;
-    U1STA = 0;
-
-    U1MODEbits.BRGH = 0; //16xbaud
-    U1BRG = (FCY / (16 * 9600)) - 1;
-
-    U1MODEbits.RTSMD = 1; //No flow control
-
-    U1MODEbits.PDSEL = 0; //8bit noParuty
-    U1MODEbits.STSEL = 0; //Stpbit 1
-
-    U1STAbits.UTXEN = 0; //Tx disnable
-    U1STAbits.URXEN = 1; //Rx enable
-
-    U1MODEbits.UEN = 0; //NO CTS  & NO RTS
-    U1MODEbits.ON = 1;
-
-    IFS1bits.U1RXIF = 0;
-    IEC1bits.U1RXIE = 1;
-
-    IPC8bits.U1IP = 4; //Interrupts Priority
-
-}
-
-unsigned char dataPos;
-unsigned char myData[2];
-unsigned char lastData;
 
 void __ISR(_UART_1_VECTOR, IPL4) U1RXHandler(void) {
     unsigned char RcvData;
@@ -256,7 +162,6 @@ int main(void) {
                 setPattern(broken[broken_frame[aCnt]], 1);
                 break;
 
-
                 //BATSU
                 //DELETE
             case 'X':
@@ -284,14 +189,9 @@ int main(void) {
 
                 //STK-L LEFT
             case 'g':
-                //WAVE
-                if (frameCount % 3 == 0) {
-                    aCnt++;
-                    if (aCnt >= sizeof (wave_frame) / sizeof (unsigned char)) {
-                        aCnt = 0;
-                    }
-                }
-                setPattern(wave[wave_frame[aCnt]], 1);
+                //nothing
+                 myData[0] = 0;
+                deletePattern();
 
                 break;
                 //STK-L RIGHT
@@ -356,23 +256,31 @@ int main(void) {
 
                 //STK-R UP
             case 'm':
-                //teapot
-                if (frameCount % 3 == 0) {
+                //hoshinoki
+                if (frameCount % 4 == 0) {
                     frameCount = 0;
                     aCnt++;
-                    if (aCnt >= sizeof (frame_teapot) / sizeof (unsigned char)) {
-                        aCnt = sizeof (frame_teapot) / sizeof (unsigned char) - 1;
+                    if (aCnt >= sizeof (frame_hoshinoki) / sizeof (unsigned char)) {
+                        aCnt = sizeof (frame_hoshinoki) / sizeof (unsigned char) - 1;
+                        myData[0] = 0;
+                        deletePattern();
                     }
                 }
-                setPattern(teapot[frame_teapot[aCnt]], 2);
+                setPattern(hoshinoki[frame_hoshinoki[aCnt]], 2);
 
                 break;
 
                 //STK-R DOWN
             case 'n':
-                //nothing
-                myData[0] = 0;
-                deletePattern();
+                //doripon
+                if (frameCount % 3 == 0) {
+                    frameCount = 0;
+                    aCnt++;
+                    if (aCnt >= sizeof (frame_doripon) / sizeof (unsigned char)) {
+                        aCnt = sizeof (frame_doripon) / sizeof (unsigned char) - 1;
+                    }
+                }
+                setPattern(doripon[frame_doripon[aCnt]], 3);
 
                 break;
         }
@@ -383,455 +291,3 @@ int main(void) {
     }
 }
 
-void setPattern(const unsigned char* ptn, unsigned char div) {
-    unsigned char x, y;
-
-    for (y = 0; y < 16; y++) {
-        for (x = 0; x < 16; x++) {
-
-            myRed = ptn[x + y * 16] >> div;
-            myGrn = ptn[256 + x + y * 16] >> div;
-            myBlu = ptn[512 + x + y * 16] >> div;
-
-            setPixelColor((x * 2) + (y * 64), myRed, myGrn, myBlu);
-            setPixelColor((x * 2) + (y * 64) + 1, myRed, myGrn, myBlu);
-            setPixelColor(63 + (y * 64) - (x * 2), myRed, myGrn, myBlu);
-            setPixelColor(63 + (y * 64) - (x * 2 + 1), myRed, myGrn, myBlu);
-        }
-    }
-
-
-}
-
-void setPixelColor(unsigned int pixel, unsigned char r, unsigned char g, unsigned char b) {
-
-    red[pixel] = r;
-    grn[pixel] = g;
-    blu[pixel] = b;
-}
-
-void show() {
-    unsigned int i;
-
-    INTDisableInterrupts();
-
-    neopixel_pin = 0;
-    for (i = 0; i < 5000; i++) {
-        Nop();
-    }
-
-    for (i = 0; i < 1024; i++) {
-        //Bit7
-        if (grn[i] & 0b10000000) {
-            neopixel_pin = 1;
-            Neopixel700us();
-
-            neopixel_pin = 0;
-            Neopixel600us();
-
-        } else {
-            neopixel_pin = 1;
-            Neopixel350us();
-            neopixel_pin = 0;
-            Neopixel700us();
-
-        }
-
-        //Bit6
-        if (grn[i] & 0b01000000) {
-            neopixel_pin = 1;
-            Neopixel700us();
-
-            neopixel_pin = 0;
-            Neopixel600us();
-
-        } else {
-            neopixel_pin = 1;
-            Neopixel350us();
-            neopixel_pin = 0;
-            Neopixel700us();
-
-        }
-
-        //Bit5
-        if (grn[i] & 0b00100000) {
-            neopixel_pin = 1;
-            Neopixel700us();
-
-            neopixel_pin = 0;
-            Neopixel600us();
-
-        } else {
-            neopixel_pin = 1;
-            Neopixel350us();
-            neopixel_pin = 0;
-            Neopixel700us();
-
-        }
-
-        //Bit4
-        if (grn[i] & 0b00010000) {
-            neopixel_pin = 1;
-            Neopixel700us();
-
-            neopixel_pin = 0;
-            Neopixel600us();
-
-        } else {
-            neopixel_pin = 1;
-            Neopixel350us();
-            neopixel_pin = 0;
-            Neopixel700us();
-
-        }
-
-        //Bit3
-        if (grn[i] & 0b00001000) {
-            neopixel_pin = 1;
-            Neopixel700us();
-
-            neopixel_pin = 0;
-            Neopixel600us();
-
-        } else {
-            neopixel_pin = 1;
-            Neopixel350us();
-            neopixel_pin = 0;
-            Neopixel700us();
-
-        }
-
-        //Bit2
-        if (grn[i] & 0b00000100) {
-            neopixel_pin = 1;
-            Neopixel700us();
-
-            neopixel_pin = 0;
-            Neopixel600us();
-
-        } else {
-            neopixel_pin = 1;
-            Neopixel350us();
-            neopixel_pin = 0;
-            Neopixel700us();
-
-        }
-
-        //Bit1
-        if (grn[i] & 0b00000010) {
-            neopixel_pin = 1;
-            Neopixel700us();
-
-            neopixel_pin = 0;
-            Neopixel600us();
-
-        } else {
-            neopixel_pin = 1;
-            Neopixel350us();
-            neopixel_pin = 0;
-            Neopixel700us();
-
-        }
-        //Bit0
-        if (grn[i] & 0b00000001) {
-            neopixel_pin = 1;
-            Neopixel700us();
-
-            neopixel_pin = 0;
-            Neopixel600us();
-
-        } else {
-            neopixel_pin = 1;
-            Neopixel350us();
-            neopixel_pin = 0;
-            Neopixel700us();
-
-        }
-
-
-        //Bit7
-        if (red[i] & 0b10000000) {
-            neopixel_pin = 1;
-            Neopixel700us();
-
-            neopixel_pin = 0;
-            Neopixel600us();
-
-        } else {
-            neopixel_pin = 1;
-            Neopixel350us();
-            neopixel_pin = 0;
-            Neopixel700us();
-
-        }
-
-        //Bit6
-        if (red[i] & 0b01000000) {
-            neopixel_pin = 1;
-            Neopixel700us();
-
-            neopixel_pin = 0;
-            Neopixel600us();
-
-        } else {
-            neopixel_pin = 1;
-            Neopixel350us();
-            neopixel_pin = 0;
-            Neopixel700us();
-
-        }
-
-        //Bit5
-        if (red[i] & 0b00100000) {
-            neopixel_pin = 1;
-            Neopixel700us();
-            neopixel_pin = 0;
-            Neopixel600us();
-
-        } else {
-            neopixel_pin = 1;
-            Neopixel350us();
-            neopixel_pin = 0;
-            Neopixel700us();
-
-        }
-
-        //Bit4
-        if (red[i] & 0b00010000) {
-            neopixel_pin = 1;
-            Neopixel700us();
-
-            neopixel_pin = 0;
-            Neopixel600us();
-
-        } else {
-            neopixel_pin = 1;
-            Neopixel350us();
-            neopixel_pin = 0;
-            Neopixel700us();
-
-        }
-
-        //Bit3
-        if (red[i] & 0b00001000) {
-            neopixel_pin = 1;
-            Neopixel700us();
-
-            neopixel_pin = 0;
-            Neopixel600us();
-
-        } else {
-            neopixel_pin = 1;
-            Neopixel350us();
-            neopixel_pin = 0;
-            Neopixel700us();
-
-        }
-
-        //Bit2
-        if (red[i] & 0b00000100) {
-            neopixel_pin = 1;
-            Neopixel700us();
-
-            neopixel_pin = 0;
-            Neopixel600us();
-
-        } else {
-            neopixel_pin = 1;
-            Neopixel350us();
-            neopixel_pin = 0;
-            Neopixel700us();
-
-        }
-
-        //Bit1
-        if (red[i] & 0b00000010) {
-            neopixel_pin = 1;
-            Neopixel700us();
-
-            neopixel_pin = 0;
-            Neopixel600us();
-
-        } else {
-            neopixel_pin = 1;
-            Neopixel350us();
-            neopixel_pin = 0;
-            Neopixel700us();
-
-        }
-        //Bit0
-        if (red[i] & 0b00000001) {
-            neopixel_pin = 1;
-            Neopixel700us();
-
-            neopixel_pin = 0;
-            Neopixel600us();
-
-        } else {
-            neopixel_pin = 1;
-            Neopixel350us();
-            neopixel_pin = 0;
-            Neopixel700us();
-
-        }
-
-
-
-
-
-
-        //Bit7
-        if (blu[i] & 0b10000000) {
-            neopixel_pin = 1;
-            Neopixel700us();
-
-            neopixel_pin = 0;
-            Neopixel600us();
-
-        } else {
-            neopixel_pin = 1;
-            Neopixel350us();
-            neopixel_pin = 0;
-            Neopixel700us();
-
-        }
-
-        //Bit6
-        if (blu[i] & 0b01000000) {
-            neopixel_pin = 1;
-            Neopixel700us();
-
-            neopixel_pin = 0;
-            Neopixel600us();
-
-        } else {
-            neopixel_pin = 1;
-            Neopixel350us();
-            neopixel_pin = 0;
-            Neopixel700us();
-
-        }
-
-        //Bit5
-        if (blu[i] & 0b00100000) {
-            neopixel_pin = 1;
-            Neopixel700us();
-
-            neopixel_pin = 0;
-            Neopixel600us();
-
-        } else {
-            neopixel_pin = 1;
-            Neopixel350us();
-            neopixel_pin = 0;
-            Neopixel700us();
-
-        }
-
-        //Bit4
-        if (blu[i] & 0b00010000) {
-            neopixel_pin = 1;
-            Neopixel700us();
-
-            neopixel_pin = 0;
-            Neopixel600us();
-
-        } else {
-            neopixel_pin = 1;
-            Neopixel350us();
-            neopixel_pin = 0;
-            Neopixel700us();
-
-        }
-
-        //Bit3
-        if (blu[i] & 0b00001000) {
-            neopixel_pin = 1;
-            Neopixel700us();
-
-            neopixel_pin = 0;
-            Neopixel600us();
-
-        } else {
-            neopixel_pin = 1;
-            Neopixel350us();
-            neopixel_pin = 0;
-            Neopixel700us();
-
-        }
-
-        //Bit2
-        if (blu[i] & 0b00000100) {
-            neopixel_pin = 1;
-            Neopixel700us();
-
-            neopixel_pin = 0;
-            Neopixel600us();
-
-        } else {
-            neopixel_pin = 1;
-            Neopixel350us();
-            neopixel_pin = 0;
-            Neopixel700us();
-
-        }
-
-        //Bit1
-        if (blu[i] & 0b00000010) {
-            neopixel_pin = 1;
-            Neopixel700us();
-
-            neopixel_pin = 0;
-            Neopixel600us();
-
-        } else {
-            neopixel_pin = 1;
-            Neopixel350us();
-            neopixel_pin = 0;
-            Neopixel700us();
-
-        }
-        //Bit0
-        if (blu[i] & 0b00000001) {
-            neopixel_pin = 1;
-            Neopixel700us();
-
-            neopixel_pin = 0;
-            Neopixel600us();
-
-        } else {
-
-            neopixel_pin = 1;
-            Neopixel350us();
-            neopixel_pin = 0;
-            Neopixel700us();
-
-        }
-    }
-
-    INTEnableInterrupts();
-
-}
-
-void deletePattern() {
-    int i;
-    for (i = 0; i < 1024; i++) {
-
-        red[i] = 0;
-        grn[i] = 0;
-        blu[i] = 0;
-    }
-}
-
-void resetAnimation() {
-    int i = 0;
-
-    for (i = 0; i < 1024; i++) {
-        red[i] = 0;
-        grn[i] = 0;
-        blu[i] = 0;
-    }
-    frameCount = 0;
-    aCnt = 0;
-
-}
